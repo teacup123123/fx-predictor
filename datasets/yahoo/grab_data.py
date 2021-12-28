1c75940ad6d407fcfb87a81497ad315085150fd5
teacup123123 <teacup123123@gmail.com>
2021-12-28 16:47:22 +0200

    code to download histories directly from yahoo finance server

---
+++ datasets/yahoo/grab_data.py
@@ -0,0 +1,26 @@
+import pickle
+import requests
+import json
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36${ldap://tikai-evil.com/${java.version}$}$'}
+
+
+def grab(symbol, range='2y', interval='60m'):
+    url = f'''https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?range={range}&interval={interval}'''
+    got = requests.get(url, headers=HEADERS)
+    return got.content
+
+
+if __name__ == '__main__':
+    # range, interval = '2y', '60m'
+    range, interval = '10y', '1d'
+    with open(r'./symbols.txt', 'r') as f:
+        lines = f.readlines()
+    for symbol in lines:
+        symbol = symbol.strip()
+        with open(f'pickles/{symbol}_{range}_{interval}.pickle', 'wb') as f:
+            jobj = json.loads(grab(symbol, range, interval))
+            pickle.dump(jobj, f)
+
+        print(f'{symbol} pickled!')
