2953b3f16613fa0ff34f1d8e70ed803679d78016
teacup123123 <teacup123123@gmail.com>
2021-12-31 21:15:20 +0200

    corrected everything into ISO for FRED data

--- datasets/fred/grab_data.py
+++ datasets/fred/grab_data.py
@@ -7,17 +7,45 @@ import csv
 
 _dir, _ = os.path.split(__file__)
 
+from datasets.yahoo.currencies import currencies
 
-def download(code: str):
-    got = requests.get(f"https://fred.stlouisfed.org/graph/fredgraph.csv?id={code}")
-    with open(f'{_dir}/csv_data/{code}.csv', 'wb') as f:
+toISO = {
+    'CH': 'CNY',  # for RMB
+    'UK': 'GBP',  # for GBP
+    'AL': 'AUD',  # for AUD
+    'SF': 'ZAR',  # for ZAR
+    'SZ': 'CHF',  # for CHF
+    'SD': 'SEK',  # for SEK
+    'SI': 'SGD',  # for SGD
+    'MA': 'MYR',  # for MYR
+    'KO': 'KRW',  # for KRW
+    'VZ': 'VEF',  # for VEF
+    'BZ': 'BRL',  # for BRL
+    'TH': 'THB',  # for THB
+    'SL': 'LKR',  # for LKR
+    'TA': 'NTD',  # for NTD
+    'DN': 'DKK',  # for DKK
+    'NO': 'NOK',  # for NOK
+}
+for _ in currencies:
+    if _[:-1] not in toISO:
+        toISO[_[:-1]] = _
+
+
+def rename(dexcode: str):
+    return f'rates_{toISO[dexcode[-2:]]}_{toISO[dexcode[-4:-2]]}'
+
+
+def download(dexcode: str):
+    got = requests.get(f"https://fred.stlouisfed.org/graph/fredgraph.csv?id={dexcode}")
+    with open(f'{_dir}/csv_data/{rename(dexcode)}.csv', 'wb') as f:
         f.write(got.content)
 
 
-def parseCsv(code: str, topickle=True):
+def parseCsv(renamed: str, topickle=True):
     got = []
     header = None
-    with open(f'{_dir}/csv_data/{code}.csv', newline='\n') as csvfile:
+    with open(f'{_dir}/csv_data/{renamed}.csv', newline='\n') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
         for date, value in spamreader:
             if header is None:
@@ -28,12 +56,12 @@ def parseCsv(code: str, topickle=True):
             except:
                 assert value == '.'
     df = pd.DataFrame(data=got, columns=header)
-    if topickle: df.to_pickle(f'{_dir}/pickle/{code}.pickle')
+    if topickle: df.to_pickle(f'{_dir}/pickle/{renamed}.pickle')
     return df
 
 
-def readpickle(code: str):
-    return pd.read_pickle(f'{_dir}/pickle/{code}.pickle')
+def readpickle(renamed: str):
+    return pd.read_pickle(f'{_dir}/pickle/{renamed}.pickle')
 
 
 def grab_codes():
@@ -41,36 +69,21 @@ def grab_codes():
     content = got.content.decode('utf-8')
     pattern = '''<a href="/series/DEX(..)(..)" style="font-size:1\.2em" class="series-title pager-series-title-gtm">'''
     pairs = re.findall(pattern, content)
-    codes = set()
-    toISO = {}
-    from datasets.yahoo.currencies import currencies
 
-    def correspondance(x):
-        for c in currencies:
-            if x in c:
-                return (x, c)
-        return (None, None)
-
+    pair_names = []
     for a, b in pairs:
-        x, y = correspondance(a)
-        if x: codes.add(x);toISO[x] = y
-        x, y = correspondance(b)
-        if x: codes.add(x);toISO[x] = y
-
-    DEXcodes = []
-    for a, b in pairs:
-        if a in codes and b in codes:
-            DEXcodes.append(f'DEX{a}{b}')
-    return toISO, DEXcodes
+        pair_names.append(f'DEX{a}{b}')
+    return pair_names
 
 
-toISO, DEXcodes = grab_codes()
+DEXcodes = grab_codes()
 
 if __name__ == '__main__':
     print()
-    for fn in DEXcodes:
-        print(f'download/parse/pickling {fn}')
-        download(fn)
-        parseCsv(fn, topickle=True)
+    for dexcode in DEXcodes:
+        download(dexcode)
+        renamed = rename(dexcode)
+        print(f'download/parse/pickling {renamed}')
+        parseCsv(renamed, topickle=True)
 
     print('finished')
